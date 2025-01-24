f0887ef52f1826a83dc3cf078c4dedbdbd8ef78e
Meth Munindradasa <methsndr@gmail.com>
2025-01-24 22:32:12 +1100

    Cloning and backups

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
-echo "YAY ITS RUNNING!!!"
+echo "Setting up Neovim..."
+
+# Deps
+# Neovim >= 0.8
+# Git >= 2.19
+# Luarocks
+# Nerdfont (optional)
+# Python 3
+# pip
+# node >= 22lts
+
+# TODO: Check for deps
+# TODO: Install deps
+
+CONF_REPO="https://github.com/muffledMitosis/nvim.git"
+BACKUP_DIR="/home/$USER/.config/nvim.back"
+CONF_DIR="/home/$USER/.config/nvim"
+
+if [ -d "$BACKUP_DIR" ]; then
+  rm -rf "$BACKUP_DIR"
+fi
+
+echo "Cloning config"
+rm -rf "$CONF_DIR"
+git clone "$CONF_REPO" "$CONF_DIR"
+
+echo "Creating new backup"
+cp -r "$CONF_DIR" "$BACKUP_DIR"
